20c13085b59e0923b5668f5f320c6ea4c70a3a68
exhale <dtzxporter@gmail.com>
2017-05-04 19:54:06 -0400

    Test definitions

---
+++ data/en.js
@@ -0,0 +1,19 @@
+{
+    0 : [
+        { date: "IN THE BEGINNING", event: "There was only the Aether and the Keepers. Among them were two beings who would later be known by many names. One would be known as Doctor Monty, the other as the Shadowman" },
+        { date: "SEPTEMBER 3RD, 5AD", event: "Knowing the planet has a gateway to Agartha, the Apothicons send meteors of Element 115 to Earth. They believe humanity will one day use Element 115 to wage war amongst themselves, opening a rift that will allow the Apothicons to escape the Dark Aether." },
+        { date: "JANUARY 15TH, 1292", event: "The Great War between humanity and the Apothicons begins." },
+        { date: "APRIL 14TH, 1294", event: "Sir Pablo Marinus is saved from the clutches of a Margwa by the four unknown heroes. They would later become known as Primis." }
+    ],
+    1 : [
+        { date: "DECEMBER 32ST, 1299", event: "Together with the Keepers, Primis defeats the Apothicons, bringing the Great War to an end." },
+        { date: "JANUARY 1ST, 1300", event: "Before they seemingly disappear from history, Primis instructs the Wolf King to begin building Der Eisendrache." },
+        { date: "SEPTEMBER 19TH, 1318", event: "Honoring the Wolf King's dying request, his loyal servant Arthur scatters and buries his bones in the grounds of Der Eisendrache, accompanied by the King's Wolf." },
+        { date: "SEPTEMBER 20th, 1318", event: "Temporal Rifts teleport Arthur to Resolution 1295 in 2025 Angola." },
+        { date: "JUNE 30TH, 1908", event: "A meteor containing Element 115 crashes near the Stony Tunguska River." }
+    ],
+    2 : [
+        { date: "AUGUST 30TH, 1925", event: "Doctor Edward Richtofen joins the Illuminati." },
+        { date: "FEBRUARY 4TH, 1931", event: "Large deposits of Element 115 are discovered near Breslau Germany. Doctor Ludvig Maxis, a german scientist, is sent to investigate." }
+    ]
+}
--- scripts/run.js
+++ scripts/run.js
@@ -1,23 +1,5 @@
 // The book data (Starts from first page (right hand side))
-var KronoriumSource = {
-    0 : [
-        { date: "IN THE BEGINNING", event: "There was only the Aether and the Keepers. Among them were two beings who would later be known by many names. One would be known as Doctor Monty, the other as the Shadowman" },
-        { date: "SEPTEMBER 3RD, 5AD", event: "Knowing the planet has a gateway to Agartha, the Apothicons send meteors of Element 115 to Earth. They believe humanity will one day use Element 115 to wage war amongst themselves, opening a rift that will allow the Apothicons to escape the Dark Aether." },
-        { date: "JANUARY 15TH, 1292", event: "The Great War between humanity and the Apothicons begins." },
-        { date: "APRIL 14TH, 1294", event: "Sir Pablo Marinus is saved from the clutches of a Margwa by the four unknown heroes. They would later become known as Primis." }
-    ],
-    1 : [
-        { date: "DECEMBER 32ST, 1299", event: "Together with the Keepers, Primis defeats the Apothicons, bringing the Great War to an end." },
-        { date: "JANUARY 1ST, 1300", event: "Before they seemingly disappear from history, Primis instructs the Wolf King to begin building Der Eisendrache." },
-        { date: "SEPTEMBER 19TH, 1318", event: "Honoring the Wolf King's dying request, his loyal servant Arthur scatters and buries his bones in the grounds of Der Eisendrache, accompanied by the King's Wolf." },
-        { date: "SEPTEMBER 20th, 1318", event: "Temporal Rifts teleport Arthur to Resolution 1295 in 2025 Angola." },
-        { date: "JUNE 30TH, 1908", event: "A meteor containing Element 115 crashes near the Stony Tunguska River." }
-    ],
-    2 : [
-        { date: "AUGUST 30TH, 1925", event: "Doctor Edward Richtofen joins the Illuminati." },
-        { date: "FEBRUARY 4TH, 1931", event: "Large deposits of Element 115 are discovered near Breslau Germany. Doctor Ludvig Maxis, a german scientist, is sent to investigate." }
-    ]
-};
+var KronoriumSource = '';
 
 // General rule of thumb: Format so we have a left and a right page before the end, so it looks natural.
 
@@ -25,19 +7,37 @@ var KronoriumSource = {
 // TODO: The rest of the damn book
 
 // Load sounds (Open / close / flip)
-var OpenSound = new Howl({
-    src: ['sound/open.mp3', 'sound/open.wav', 'sound/open.ogg'],
-    onload: function() { SetupInitialAnim(); }
-});
-var CloseSound = new Howl({
-    src: ['sound/close.mp3', 'sound/close.wav', 'sound/close.ogg']
-});
-var FlipSound = new Howl({
-    src: ['sound/flip.mp3', 'sound/flip.wav', 'sound/flip.ogg']
-});
+var OpenSound = null;
+var CloseSound = null;
+var FlipSound = null;
 
 $(document).ready(function()
 {
+    // We must load the specific JSON source for our language (EN for now)
+    $.get('data/en.js', function(data)
+    {
+        // Set it
+        KronoriumSource = JSON.parse(data);
+        // Setup page
+        SetupPage();
+        // Load sounds
+        OpenSound = new Howl({
+            src: ['sound/open.mp3', 'sound/open.wav', 'sound/open.ogg'],
+            onload: function() { SetupInitialAnim(); }
+        });
+        CloseSound = new Howl({
+            src: ['sound/close.mp3', 'sound/close.wav', 'sound/close.ogg']
+        });
+        FlipSound = new Howl({
+            src: ['sound/flip.mp3', 'sound/flip.wav', 'sound/flip.ogg']
+        });
+    }).fail(function()
+    {
+        // TODO: Display we had an issue loading the language definitions
+    });
+});
+
+function SetupPage() {
     // Inject pages based on the template
     $('#kronorium').append('<div class="hard" style="background-image:url(images/binding_front.png)"></div>');
     // Actual binding
@@ -88,7 +88,7 @@ $(document).ready(function()
 	});
     // Disable it for the opening effect
     $("#kronorium").turn("disable", true);
-});
+}
 
 function SetupInitialAnim() {
     // Add the shake effect, play sound, then countdown to open
